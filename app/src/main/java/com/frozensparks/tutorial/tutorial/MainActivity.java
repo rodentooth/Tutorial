74e0c5f8deae438268b9353f7a20173a828b6ee9
rodentooth <emanuel.smile@hotmail.com>
2016-04-02 22:25:25 +0200

    Initial commit

---
+++ app/src/main/java/com/frozensparks/tutorial/tutorial/MainActivity.java
@@ -0,0 +1,56 @@
+package com.frozensparks.tutorial.tutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    ProgressBar pgb;
+    TextView txt;
+    int progressbarstatus = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+
+
+        pgb = (ProgressBar) findViewById(R.id.pgb);
+        txt = (TextView) findViewById(R.id.tw);
+
+
+        pgb.setProgress(0);
+        pgb.setMax(100);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pgb.setProgress(pgb.getProgress()+10);
+
+                progressbarstatus = progressbarstatus+10;
+
+                if (progressbarstatus >= 100){
+
+
+                    txt.setText("DONE!");
+
+                }
+
+
+            }
+        });
+
+
+    }
+
+
+}
